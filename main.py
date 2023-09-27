a65c0a4b1cbe91dbdfd25b791ee718c9e42ce84f
Mtv28 <meremkulovtv@gmail.com>
2023-09-27 08:57:18 +0300

    bot

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, F
 import asyncio
+from aiogram.types import Message
+
+bot = Bot(token='6247171392:AAEQ1gb798E2DLx9IvLJ1yxgGs3du-OHekE')
+dp = Dispatcher()
 
 
+@dp.message(F.text == '/start')
+async def cmd_start(message: Message):
+    await message.answer('Добро пожаловать')
 
-token = '6247171392:AAEQ1gb798E2DLx9IvLJ1yxgGs3du-OHekE'
-async def start(message: Message):
+
+async def main():
+    await dp.start_polling(bot)
 
 
+asyncio.run((main()))
 
+if __name__== '__main__':
+    asyncio.run(main())
 
