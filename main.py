bb7ee5304b37494d37e0f9a4c6f71a4406d1ca32
mtv <meremkulovtv@gmail.com>
2023-09-27 19:13:34 +0300

    Greate bot

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from aiogram.types import Message
 from app.handlers import router
 
 
-
 async def main():
     bot = Bot(token='6247171392:AAEQ1gb798E2DLx9IvLJ1yxgGs3du-OHekE')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
-
-asyncio.run((main()))
 
 if __name__ == '__main__':
     asyncio.run(main())
