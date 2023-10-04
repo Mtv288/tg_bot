5d91b8e3a61bac09bcc43f927c99c36dadea2287
Mtv28 <meremkulovtv@gmail.com>
2023-10-04 09:05:15 +0300

    Greate Bot

--- bot/data_base/data_base_main.py
+++ bot/data_base/data_base_main.py
@@ -1,18 +1,25 @@
 import sqlalchemy as db
-
+import sqlite3
 
 engine = db.create_engine('sqlite:///data_all.db')
-connection = engine.connect()
+conn = engine.connect()
 metadata = db.MetaData()
 
-product = db.Table('all', metadata,
-                   db.Column('id', db.Integer, primary_key=True),
-                   db.Column('code', db.Integer),
-                   db.Column('name', db.String),
-                   db.Column('photo', db.String),
-                   db.Column('price', db.Integer),
-                   db.Column('quantity', db.Integer),
-                   db.Column('size', db.Integer)
-                   )
+data_all = db.Table('data_all', metadata,
+                    db.Column('id', db.Integer, primary_key=True),
+                    db.Column('code', db.Integer),
+                    db.Column('name', db.Text),
+                    db.Column('photo', db.Text),
+                    db.Column('price', db.Integer),
+                    db.Column('quantity', db.Integer),
+                    db.Column('size', db.Integer)
+                    )
 
 metadata.create_all(engine)
+insertion_query = data_all.insert().values([{'code': 546464, 'name': 'gfdgdfg', 'photo': 'dghdh',
+                                             'price': 5465, 'quantity': 45346767, 'size': 346546}])
+
+conn.execute(insertion_query)
+select_all_query = db.select([data_all])
+select_all_result = conn.execute(select_all_query)
+print(select_all_result.all())
