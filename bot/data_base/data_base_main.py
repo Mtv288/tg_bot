2dc284f1cde1014dcd2c705456ac37259b33760e
Mtv28 <meremkulovtv@gmail.com>
2023-10-13 15:14:24 +0300

    Greate Bot

--- bot/data_base/data_base_main.py
+++ bot/data_base/data_base_main.py
@@ -7,15 +7,13 @@ engine = create_engine('sqlite:///data_all.db')
 metadata = MetaData()
 
 Base.metadata.create_all(engine)
-session = Session(engine)
 
 with open(r'C:\TrueShop2site\All.csv') as exs:
     reader = csv.DictReader(exs, delimiter=";")
+    with Session(engine) as session:
+        for i in reader:
+            user = AllData(code=i['code'], name=i['name'], photo=i['photo'],
+                           price=i['price'], quantity=i['quantity'], size=i['Размер'])
 
-    for i in reader:
-        user = AllData(code=i['code'], name=i['name'], photo=i['photo'],
-                       price=i['price'], quantity=i['quantity'], size=i['Размер'])
-
-        session.add(user)
-    session.commit()
-    session.close()
+            session.add(user)
+        session.commit()
