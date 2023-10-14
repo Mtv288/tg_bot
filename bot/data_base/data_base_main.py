4415df2f658c314c383f9f4a75552610f5164b3b
Mtv28 <meremkulovtv@gmail.com>
2023-10-14 11:36:42 +0300

    Greate Bot

--- bot/data_base/data_base_main.py
+++ bot/data_base/data_base_main.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
-from table_models import AllData, Base
+from table_models import AllData, Base, MenShoes, WomenShoes
 import csv
 
 engine = create_engine('sqlite:///data_all.db')
@@ -12,8 +12,22 @@ with open(r'C:\TrueShop2site\All.csv') as exs:
     reader = csv.DictReader(exs, delimiter=";")
     with Session(engine) as session:
         for i in reader:
-            user = AllData(code=i['code'], name=i['name'], photo=i['photo'],
+            user = AllData(code=i['code'], group_code=i['group_code'], name=i['name'], photo=i['photo'],
                            price=i['price'], quantity=i['quantity'], size=i['Размер'])
 
             session.add(user)
         session.commit()
+
+with Session(engine) as session:
+    for i in session.query(AllData).filter(AllData.name.like("МУЖ%")).filter(AllData.quantity > 0):
+        men_shoes = MenShoes(code=i.code, group_code=i.group_code, name=i.name, photo=i.photo,
+                             price=i.price, quantity=i.quantity, size=i.size)
+        session.add(men_shoes)
+    session.commit()
+
+with Session(engine) as session:
+    for i in session.query(AllData).filter(AllData.name.like("ЖЕН%")).filter(AllData.quantity > 0):
+        women_shoes = WomenShoes(code=i.code, group_code=i.group_code, name=i.name, photo=i.photo,
+                             price=i.price, quantity=i.quantity, size=i.size)
+        session.add(women_shoes)
+    session.commit()
--- bot/data_base/table_models.py
+++ bot/data_base/table_models.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import String
+
 
 
 class Base(DeclarativeBase):
@@ -10,11 +12,36 @@ class AllData(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     code: Mapped[int] = mapped_column(default=0)
-    name: Mapped[str] = mapped_column(default='0')
+    group_code: Mapped[int]
+    name: Mapped[str] = mapped_column(String(25), default='0')
     photo: Mapped[str] = mapped_column(default='0')
     price: Mapped[int] = mapped_column(default=0)
     quantity: Mapped[int] = mapped_column(default=0)
     size: Mapped[int] = mapped_column(default=0)
 
 
+class MenShoes(Base):
+    __tablename__ = 'men_shoes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_code: Mapped[int]
+    code: Mapped[int]
+    name: Mapped[str]
+    photo: Mapped[str]
+    price: Mapped[int]
+    quantity: Mapped[int]
+    size: Mapped[int]
+
+
+class WomenShoes(Base):
+    __tablename__ = 'women_shoes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_code: Mapped[int]
+    code: Mapped[int]
+    name: Mapped[str]
+    photo: Mapped[str]
+    price: Mapped[int]
+    quantity: Mapped[int]
+    size: Mapped[int]
 
