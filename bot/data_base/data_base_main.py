64f570d414d14edf4186400d58e30dda000b5714
Mtv28 <meremkulovtv@gmail.com>
2023-10-14 11:51:43 +0300

    Greate Bot

--- bot/data_base/data_base_main.py
+++ bot/data_base/data_base_main.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
-from table_models import AllData, Base, MenShoes, WomenShoes
+from table_models import AllData, Base, MenShoes, WomenShoes, ChildrenShoes, MenSlippers, WomenSlippers
 import csv
 
 engine = create_engine('sqlite:///data_all.db')
@@ -28,6 +28,34 @@ with Session(engine) as session:
 with Session(engine) as session:
     for i in session.query(AllData).filter(AllData.name.like("ЖЕН%")).filter(AllData.quantity > 0):
         women_shoes = WomenShoes(code=i.code, group_code=i.group_code, name=i.name, photo=i.photo,
-                             price=i.price, quantity=i.quantity, size=i.size)
+                                 price=i.price, quantity=i.quantity, size=i.size)
         session.add(women_shoes)
+    session.commit()
+
+with Session(engine) as session:
+    for i in session.query(AllData).filter(AllData.name.like("ДЕТ%")).filter(AllData.quantity > 0):
+        children_shoes = ChildrenShoes(code=i.code, group_code=i.group_code, name=i.name, photo=i.photo,
+                                       price=i.price, quantity=i.quantity, size=i.size)
+        session.add(children_shoes)
+    session.commit()
+
+with Session(engine) as session:
+    for i in session.query(AllData).filter(AllData.name.like("ДЕТ%")).filter(AllData.quantity > 0):
+        children_shoes = ChildrenShoes(code=i.code, group_code=i.group_code, name=i.name, photo=i.photo,
+                                       price=i.price, quantity=i.quantity, size=i.size)
+        session.add(children_shoes)
+    session.commit()
+
+with Session(engine) as session:
+    for i in session.query(AllData).filter(AllData.name.like("%Тм%")).filter(AllData.quantity > 0):
+        men_slippers = MenSlippers(code=i.code, group_code=i.group_code, name=i.name, photo=i.photo,
+                                       price=i.price, quantity=i.quantity, size=i.size)
+        session.add(men_slippers)
+    session.commit()
+
+with Session(engine) as session:
+    for i in session.query(AllData).filter(AllData.name.like("%Тж%")).filter(AllData.quantity > 0):
+        women_slippers = WomenSlippers(code=i.code, group_code=i.group_code, name=i.name, photo=i.photo,
+                                       price=i.price, quantity=i.quantity, size=i.size)
+        session.add(women_slippers)
     session.commit()
--- bot/data_base/table_models.py
+++ bot/data_base/table_models.py
@@ -8,7 +8,7 @@ class Base(DeclarativeBase):
 
 
 class AllData(Base):
-    __tablename__ = 'new'
+    __tablename__ = 'all_goods'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     code: Mapped[int] = mapped_column(default=0)
@@ -44,4 +44,44 @@ class WomenShoes(Base):
     price: Mapped[int]
     quantity: Mapped[int]
     size: Mapped[int]
+
+
+class ChildrenShoes(Base):
+    __tablename__ = 'children_shoes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_code: Mapped[int]
+    code: Mapped[int]
+    name: Mapped[str]
+    photo: Mapped[str]
+    price: Mapped[int]
+    quantity: Mapped[int]
+    size: Mapped[int]
+
+
+class MenSlippers(Base):
+    __tablename__ = 'men_slippers'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_code: Mapped[int]
+    code: Mapped[int]
+    name: Mapped[str]
+    photo: Mapped[str]
+    price: Mapped[int]
+    quantity: Mapped[int]
+    size: Mapped[int]
+
+
+class WomenSlippers(Base):
+    __tablename__ = 'women_slippers'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_code: Mapped[int]
+    code: Mapped[int]
+    name: Mapped[str]
+    photo: Mapped[str]
+    price: Mapped[int]
+    quantity: Mapped[int]
+    size: Mapped[int]
+
 
