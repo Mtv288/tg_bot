994f7a909f1c5abbd36fdf8c36a04b343842cd6a
Mtv28 <meremkulovtv@gmail.com>
2023-12-02 10:49:23 +0300

    great Bot

--- bot/handlers/replybutton_hendler.py
+++ bot/handlers/replybutton_hendler.py
@@ -44,7 +44,8 @@ async def men_shoes_list(message: Message):
     await message.answer('Назад', reply_markup=reply_keyboard.return_kb)
     with Session(engine) as session:
         for i in session.query(Catalog):
-            await message.answer_photo(FSInputFile(str(i.photo)), f'Цена: {str(i.price)} р.')
+            await message.answer_photo(FSInputFile(str(i.photo)), f'Цена: {str(i.price)} р.', reply_markup=inline_keyboard.men)
+
 
 
 
