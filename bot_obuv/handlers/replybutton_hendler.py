c8bd6d563dcafc5a367ead3550a3d06d737fbcef
Mtv28 <meremkulovtv@gmail.com>
2024-01-10 15:54:04 +0300

    great Bot

--- bot_obuv/handlers/replybutton_hendler.py
+++ bot_obuv/handlers/replybutton_hendler.py
@@ -62,7 +62,7 @@ async def men_shoes_list(message: Message):
     await message.delete()
     await message.answer('Назад', reply_markup=return_kb())
     photos, price = great_list_for_media_group('МУЖ КРО')
-    count_message_from_media_group = len(photos) // 10
+    count_message_from_media_group = len(photos) / 10
     if isinstance(count_message_from_media_group, int):
         d = 0
     else:
