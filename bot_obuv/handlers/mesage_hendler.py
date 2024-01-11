ac43f91f0d367215bdfbaef8d13baf1a41245bdd
Mtv28 <meremkulovtv@gmail.com>
2024-01-11 12:42:10 +0300

    great Bot

--- bot_obuv/handlers/mesage_hendler.py
+++ bot_obuv/handlers/mesage_hendler.py
@@ -1,12 +1,36 @@
 from aiogram import F, Router
 from aiogram.types import Message
-import re
-
 
 router = Router()
 
 
 @router.message(lambda message: 'жен' in message.text.lower())
-async def hello(message: Message):
-    await message.reply(f'Найдено совпадение: ура')
+async def woman_shoes(message: Message):
+    await message.reply('Если вы хотите посмотреть ассортимент женской обуви нажмите на кнопку '
+                        '"Женская обувь" внизу экрана и выберите интересующую вас категорию обуви')
 
+
+@router.message(lambda message: 'муж' in message.text.lower())
+async def men_shoes(message: Message):
+    await message.reply('Если вы хотите посмотреть ассортимент мужской обуви нажмите на кнопку '
+                        '"Мужская обувь" внизу экрана и выберите интересующую вас категорию обуви')
+
+
+@router.message(lambda message: 'дет' in message.text.lower())
+async def kid_shoes(message: Message):
+    await message.reply('Если вы хотите посмотреть ассортимент детской обуви нажмите на кнопку '
+                        '"Детская обувь" внизу экрана и выберите интересующую вас категорию обуви')
+
+
+@router.message(lambda message: 'тап' in message.text.lower())
+async def kid_shoes(message: Message):
+    await message.reply('Если вы хотите посмотреть ассортимент тапочек нажмите на кнопку '
+                        '"Тапочки" внизу экрана и выберите интересующую вас категорию обуви')
+
+
+@router.message(lambda message: 'работа' in message.text.lower())
+async def job_time(message: Message):
+    await message.answer('Наш режим работы:'
+                         '  Понедельник - Пятница с 8.00 до 17.30'
+                         '  Суббота - Воскресенье с 9.00 до 16.00'
+                         '  без перерывов и выходных')
