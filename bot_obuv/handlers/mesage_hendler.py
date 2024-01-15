b0b14192ff6a1841afb31f0da0d1d5a6fc3a1553
Mtv28 <meremkulovtv@gmail.com>
2024-01-15 16:15:03 +0300

    great Bot

--- bot_obuv/handlers/mesage_hendler.py
+++ bot_obuv/handlers/mesage_hendler.py
@@ -3,6 +3,7 @@ from aiogram.types import Message
 
 router = Router()
 
+
 def answer_for_message(type_shoes):
     answer = f'Если вы хотите посмотреть ассортимент обуви нажмите на кнопку ' \
              f'{type_shoes} внизу экрана и выберите интересующую вас категорию обуви'
@@ -18,7 +19,6 @@ async def woman_shoes(message: Message):
 @router.message(lambda message: 'муж' in message.text.lower())
 async def men_shoes(message: Message):
     await message.reply(answer_for_message('"Мужская обувь"'))
-
 
 
 @router.message(lambda message: 'дет' in message.text.lower())
@@ -39,4 +39,3 @@ async def job_time(message: Message):
                         '  Понедельник - Пятница с 8.00 до 17.30'
                         '  Суббота - Воскресенье с 9.00 до 16.00'
                         '  без перерывов и выходных')
-
